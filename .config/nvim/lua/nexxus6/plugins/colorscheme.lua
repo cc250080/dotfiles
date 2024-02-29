0c2528c9dfa1c4c1d9d5a41beb2d99fdc3625d8f
cortescc <cortescc@filemon.carles.cc>
2024-02-29 18:06:45 +0100

    adding telescope to neovim

--- .config/nvim/lua/nexxus6/plugins/colorscheme.lua
+++ .config/nvim/lua/nexxus6/plugins/colorscheme.lua
@@ -5,6 +5,6 @@ return {
    priority = 1000,
    config = function()
 	   -- load the colorscheme here
-     vim.cmd([[colorscheme catppuccin-mocha]])
+     vim.cmd([[colorscheme catppuccin]])
    end,
 }

30426a0a21f386e7fa20ed3efe9a720e6e42e76a
cortescc <cortescc@filemon.carles.cc>
2024-02-29 18:57:36 +0100

    improving more neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,1 @@
 require("nexxus6")
-
--- BASIC VALUES
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
--- .config/nvim/lua/nexxus6/init.lua
+++ .config/nvim/lua/nexxus6/init.lua
@@ -1,2 +1,3 @@
 require("nexxus6.remap")
 require("nexxus6.lazy")
+require("nexxus6.vim-options")
